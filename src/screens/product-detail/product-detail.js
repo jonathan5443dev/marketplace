4071f26505c7860c030d9874cd2a78b2efd78831
Sergio Andres Florez <sergio.florez@grability.com>
2020-05-14 09:09:43 -0500

    remove unnecessary components in product-detail

--- src/screens/product-detail/product-detail.js
+++ src/screens/product-detail/product-detail.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 
-import FeatureModal from '../../components/feature-modal/feature-modal';
 import Text from '../../components/text/text';
 import Button from '../../components/button/button';
 import {useDispatch} from 'react-redux';
 import Container from '../../components/container/container';
 import ProductImageCarousel from '../../components/product-image-carousel/product-image-carousel';
+import Counter from '../../components/counter/counter';
 import shoppingCartActions from '../../redux/reducers/shopping-cart';
 import * as Tags from './styles';
 
@@ -16,40 +16,6 @@ import product4 from '../../assets/images/product2.png';
 
 import {useTranslation} from 'react-i18next';
 
-const sizeOptions = [
-  {
-    id: 1,
-    value: 'XS',
-  },
-  {
-    id: 2,
-    value: 'S',
-  },
-  {
-    id: 3,
-    value: 'M',
-  },
-  {
-    id: 4,
-    value: 'L',
-  },
-  {
-    id: 5,
-    value: 'XL',
-  },
-];
-
-const colorOptions = [
-  {
-    id: 1,
-    value: 'Negro',
-  },
-  {
-    id: 2,
-    value: 'Rojo',
-  },
-];
-
 const images = [
   {id: 1, resource: product1},
   {id: 1, resource: product2},
@@ -59,8 +25,6 @@ const images = [
 
 const ProductDetail = ({navigation}) => {
   const product = navigation.state.params.product;
-  const [modalSize, setModalSize] = useState(false);
-  const [modalColor, setModalColor] = useState(false);
   const {t} = useTranslation();
   const dispatch = useDispatch();
 
@@ -69,18 +33,9 @@ const ProductDetail = ({navigation}) => {
       <ProductImageCarousel images={images} />
       <Container>
         <Tags.ContentScrollable>
-          <Tags.FeatureContainer>
-            <Tags.OpenModal onPress={() => setModalSize(true)}>
-              <Text type="headline 3">{product.size}</Text>
-            </Tags.OpenModal>
-            <Tags.OpenModal onPress={() => setModalColor(true)}>
-              <Text type="headline 3">{product.color}</Text>
-            </Tags.OpenModal>
-          </Tags.FeatureContainer>
           <Tags.BasicInformation>
             <Tags.NameAndBrand>
               <Text type="headline 2">{product.name}</Text>
-              <Text type="headline 3">{product.brand}</Text>
             </Tags.NameAndBrand>
             <Tags.Price>
               <Text type="headline 3">$ {product.price}</Text>
@@ -88,22 +43,9 @@ const ProductDetail = ({navigation}) => {
           </Tags.BasicInformation>
           <Text type="description">{product.description}</Text>
         </Tags.ContentScrollable>
-        <FeatureModal
-          title="Select size"
-          options={sizeOptions}
-          onSelectOption={() => {}}
-          visible={modalSize}
-          onRequestClose={() => setModalSize(false)}
-        />
-        <FeatureModal
-          title="Select color"
-          options={colorOptions}
-          onSelectOption={() => {}}
-          visible={modalColor}
-          onRequestClose={() => setModalColor(false)}
-        />
       </Container>
       <Tags.ContainerButton>
+        <Counter product={product} />
         <Button
           onPress={() => dispatch(shoppingCartActions.AddProductFlow(product))}
           value={t('addToCart')}
